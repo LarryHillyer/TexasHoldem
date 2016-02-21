e95e9ce606c7159a3bbbcc8a28356642faf5dad4
LarryHillyer <LarryHillyer@hotmail.com>
2016-02-20 20:28:05 -0700

    Wired Base HTML into Dispatcher Pages

---
+++ gamesim/static/js/sim-dispatcher.js
@@ -0,0 +1,19 @@
+
+update_dispatcher();
+function update_dispatcher() {		
+	sim1Trigger();
+	sim2Trigger();}
+		
+function sim1Trigger() {
+	return setInterval(function(){ sim_button_click() }, 2500); }	
+function sim2Trigger() {
+	return setInterval(function(){ sim_button2_click() }, 2500); }
+	
+function sim_button_click() {		
+	var status = document.getElementById("get_status").getAttribute("class");
+	if (status != "Finished" ){
+		document.getElementById("get_status").click(); }}	
+function sim_button2_click() {			
+	var status = document.getElementById("get_status").getAttribute("class");	 
+	if (status != "Finished" ){
+		document.getElementById("get_l_status").click(); }}
